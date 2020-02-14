3ac73b3a8548d8ce75594bb7ccdf75064ffe4694
Amy Fong <amyhfong@gmail.com>
2020-02-14 19:16:19 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,5 +1,5 @@
 require 'nokogiri'
 require 'open-uri'
 
-doc = Nokorigi::HTML(open("https://flatironschool.com/"))
+doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 puts doc.css(".headline-26OIBN").text.strip
