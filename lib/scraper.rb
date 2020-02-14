fe5924db12246672dd421570b21c8977f9f0ca67
Amy Fong <amyhfong@gmail.com>
2020-02-14 19:25:00 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,5 +3,8 @@ require 'open-uri'
 
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 puts doc.css(".headline-26OIBN").text.strip
+courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
 
-puts doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
+courses.each do |course|
+  puts course.text.strip
+end
